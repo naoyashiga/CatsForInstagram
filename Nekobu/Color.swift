6c8227c017f305eeff18ebae8a7daa309efa1ba7
naoyashiga <naoyashiga0@gmail.com>
2015-08-15 13:59:11 +0900

    pageMenuの導入

---
+++ Nekobu/Color.swift
@@ -0,0 +1,56 @@
+//
+//  Color.swift
+//  Nekobu
+//
+//  Created by naoyashiga on 2015/08/15.
+//  Copyright (c) 2015年 naoyashiga. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    class func hexStr (var hexStr : NSString, var alpha : CGFloat) -> UIColor {
+        hexStr = hexStr.stringByReplacingOccurrencesOfString("#", withString: "")
+        let scanner = NSScanner(string: hexStr as String)
+        var color: UInt32 = 0
+        if scanner.scanHexInt(&color) {
+            let r = CGFloat((color & 0xFF0000) >> 16) / 255.0
+            let g = CGFloat((color & 0x00FF00) >> 8) / 255.0
+            let b = CGFloat(color & 0x0000FF) / 255.0
+            return UIColor(red:r,green:g,blue:b,alpha:alpha)
+        } else {
+            print("invalid hex string")
+            return UIColor.whiteColor();
+        }
+    }
+    
+    class func viewBackgroundColor() -> UIColor {
+        return UIColor.hexStr("e5e5e5", alpha: 1)
+    }
+    
+    class func navigationBarTintColor() -> UIColor {
+        return UIColor.hexStr("ffa633", alpha: 1)
+    }
+    
+    //MARK: Tab Color
+    class func scrollMenuBackgroundColor() -> UIColor {
+        return UIColor.hexStr("ffa633", alpha: 1)
+    }
+    
+    class func selectionIndicatorColor() -> UIColor {
+        return UIColor.hexStr("ffffff", alpha: 1)
+    }
+    
+    class func bottomMenuHairlineColor() -> UIColor {
+        return UIColor.hexStr("402908", alpha: 1)
+    }
+    
+    class func selectedMenuItemLabelColor() -> UIColor {
+        return UIColor.hexStr("402908", alpha: 1)
+    }
+    
+    class func unselectedMenuItemLabelColor() -> UIColor {
+        return UIColor.hexStr("ffffff", alpha: 1)
+    }
+}
---
+++ Nekobu/PageMenuViewController.swift
@@ -0,0 +1,67 @@
+//
+//  PageMenuViewController.swift
+//  Nekobu
+//
+//  Created by naoyashiga on 2015/08/15.
+//  Copyright (c) 2015年 naoyashiga. All rights reserved.
+//
+
+import UIKit
+
+class PageMenuViewController: UIViewController {
+    var pageMenu : CAPSPageMenu?
+    var controllerArray : [BaseCollectionViewController] = []
+    
+    private struct NibNameSet {
+        static let topVC = "TopCollectionViewController"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "ねこ部"
+        navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+        navigationController?.navigationBar.barTintColor = UIColor.navigationBarTintColor()
+        
+        let topVC = TopCollectionViewController(nibName: NibNameSet.topVC, bundle: nil)
+        
+        topVC.title = "ピックアップ"
+        
+        controllerArray.append(topVC)
+        controllerArray.append(topVC)
+        controllerArray.append(topVC)
+        controllerArray.append(topVC)
+        
+        let parameters: [CAPSPageMenuOption] = [
+            .ScrollMenuBackgroundColor(UIColor.scrollMenuBackgroundColor()),
+            .ViewBackgroundColor(UIColor.viewBackgroundColor()),
+            .SelectionIndicatorColor(UIColor.selectionIndicatorColor()),
+            //            .BottomMenuHairlineColor(UIColor.bottomMenuHairlineColor()),
+            .SelectedMenuItemLabelColor(UIColor.selectedMenuItemLabelColor()),
+            .UnselectedMenuItemLabelColor(UIColor.unselectedMenuItemLabelColor()),
+            .SelectionIndicatorHeight(2.0),
+            .MenuItemFont(UIFont(name: "HiraKakuProN-W6", size: 12.0)!),
+            .MenuHeight(30.0),
+            .MenuItemWidth(80.0),
+            .MenuMargin(0.0),
+            //            "useMenuLikeSegmentedControl": true,
+            .MenuItemSeparatorRoundEdges(true),
+            //            "enableHorizontalBounce": true,
+            //            "scrollAnimationDurationOnMenuItemTap": 300,
+            .CenterMenuItems(true)]
+        
+        pageMenu = CAPSPageMenu(viewControllers: controllerArray, frame: CGRectMake(0.0, 64.0, view.frame.width, view.frame.height), pageMenuOptions: parameters)
+        
+        if let pageMenu = pageMenu {
+            view.addSubview(pageMenu.view)
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+//    override func viewWillAppear(animated: Bool) {
+//        navigationController?.setNavigationBarHidden(true, animated: true)
+//    }
+}
